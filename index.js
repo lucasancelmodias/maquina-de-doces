a801bdf4c8ef8163fbdc2ee6e0f2eb271005b950
Lucas Ancelmo Dias <lucas.ancelmo547@gmail.com>
2021-10-16 01:08:54 -0300

    HTML de acordo com Mockup e operações básicas

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 $( window ).on( "load", function() {
-    console.log( "window loaded" );
+    
+    render()
 });
+
+function render(){
+    var saldoValor = document.getElementById("saldoValor")
+    saldoValor.innerHTML = saldo
+    //`${saldo},00`
+
+    var trocoValor = document.getElementById("trocoValor")
+    trocoValor.innerHTML = troco
+    //`${troco},00`
+}
+var doces = {
+    "A" : 6,
+    "B" : 7,
+    "C" : 8
+}
+var saldo = 0;
+var troco = 0;
+
+function adicionarSaldo(event){
+    saldo = parseInt(saldo) + parseInt(event.value)
+    render()
+}
+
+function desistir(){
+    saldo = 0
+    troco = 0
+    render()
+}
+
+function comprar(){
+    var opcao = $( "input[name=options]:checked" ).val()
+    
+    if(opcao && saldo > doces[opcao]){
+        console.log('comprando', doces[opcao])
+        saldo = parseInt(saldo) - doces[opcao]
+        console.log('saldo atual', saldo)
+        render()
+    }
+}
